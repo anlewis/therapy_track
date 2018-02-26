7f35a7b5d3f86e89b6fb49dcd47d6346226f1738
Anna Lewis <anna.noel.lewis@gmail.com>
2018-02-26 13:29:55 -0700

    Check if user has previously authorized calendar

--- db/migrate/20180226185147_add_refresh_token_to_user.rb
+++
@@ -1,5 +0,0 @@
-class AddRefreshTokenToUser < ActiveRecord::Migration[5.1]
-  def change
-    add_column :users, :refresh_token, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180226192002) do
+ActiveRecord::Schema.define(version: 20180226202823) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,7 +34,6 @@ ActiveRecord::Schema.define(version: 20180226192002) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "calendar"
-    t.string "refresh_token"
   end
 
   add_foreign_key "google_auths", "users"
