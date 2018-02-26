c09a6ea2b97d2e317b801cdeab6868b36c845bd3
Anna Lewis <anna.noel.lewis@gmail.com>
2018-02-26 14:56:26 -0700

    Cleanup comments

--- app/services/google_calendar_service.rb
+++ app/services/google_calendar_service.rb
@@ -1,13 +1,12 @@
 class GoogleCalendarService
 
-  def initialize(auth, current_user)
-    @auth = auth
+  def initialize(current_user)
     @current_user = current_user
   end
 
   def all_appointments
     service = Google::Apis::CalendarV3::CalendarService.new
-    service.authorization = auth
+    service.authorization = authorization
     if current_user.calendar.nil?
       calendar = Google::Apis::CalendarV3::Calendar.new(
         summary: 'TherapyTrack'
@@ -23,7 +22,7 @@ class GoogleCalendarService
 
   def new_appointment(event_info)
     service = Google::Apis::CalendarV3::CalendarService.new
-    service.authorization = auth
+    service.authorization = authorization
     if current_user.calendar.nil?
       calendar = Google::Apis::CalendarV3::Calendar.new(
         summary: 'TherapyTrack'
@@ -40,24 +39,18 @@ class GoogleCalendarService
       end: Google::Apis::CalendarV3::EventDateTime.new(date: today + 1),
       summary: 'New event!'
     })
-    service.insert_event(current_user.calendar, event)
+    service.insert_event(user.calendar, event)
   end
 
-  private
-    def create_appointments_calendar(auth, service)
-      # create calendar for user if necessary
-      if current_user.calendar.nil?
-        calendar = Google::Apis::CalendarV3::Calendar.new(
-          summary: 'TherapyTrack'
-        )
-        service.insert_calendar(calendar)
-
-        user_calendar = service.insert_calendar(calendar)
-
-        current_user.update!(calendar: user_calendar.id)
-      end
-    end
-
       private
-        attr_reader :auth, :current_user
+        attr_reader :current_user
+
+        def authorization
+          {
+            access_token:   current_user.google_auth.access_token,
+            expires_in:     current_user.google_auth.expires_in,
+            token_type:     current_user.google_auth.token_type,
+            refresh_token:  current_user.google_auth.refresh_token
+          }
+        end
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,15 +4,3 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['google_client_id'], ENV['google_client_secret'],
     {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
 end
-
-
-# Rails.application.config.middleware.use OmniAuth::Builder do
-#   provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"], {
-#     :name => "google",
-#     :scope => ['contacts','plus.login','plus.me','email','profile'],
-#     :prompt => "select_account",
-#     :image_aspect_ratio => "square",
-#     :image_size => 50,
-#     :access_type => 'offline'
-#   }
-# end
