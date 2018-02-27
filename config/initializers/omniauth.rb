e2bd3455869e2144fc5e08b1e5f1edcea6931204
Anna Lewis <anna.noel.lewis@gmail.com>
2018-02-26 19:53:35 -0700

    Make google oauth work with omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,10 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['google_client_id'], ENV['google_client_secret'],
-    {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
+  provider  :google_oauth2,
+            ENV['google_client_id'],
+            ENV['google_client_secret'],
+            { scope: 'email,profile,calendar',
+              provider_ignores_state: true 
+            }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: "home#show"
 
-  get 'auth/:provider/callback', to: 'sessions#create', as: 'signin'
+  get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
   get 'signout', to: 'sessions#destroy', as: 'signout'
 
@@ -10,8 +10,6 @@ Rails.application.routes.draw do
 
   resources :appointments, only: [:index, :new, :create]
 
-  get '/redirect', to: 'google_oauth#redirect', as: 'google_oauth_redirect'
-  get '/callback', to: 'google_oauth#callback', as: 'google_oauth_callback'
   get '/events/:calendar_id', to: 'appointments#events', as: 'events', calendar_id: /[^\/]+/
   post '/events/:calendar_id', to: 'appointments#new_event', as: 'new_event', calendar_id: /[^\/]+/
 end
