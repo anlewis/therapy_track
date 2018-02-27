38a09b633b07b1fecf5ac2ed0e0e87902eb33818
Anna Lewis <anna.noel.lewis@gmail.com>
2018-02-27 15:17:39 -0700

    Make test pass

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
   factory :user do
-    provider "MyString"
+    provider "google_oauth2"
     uid ENV['uid']
     name "Anna Lewis"
     oauth_token ENV['oauth_token']
-    oauth_expires_at "2018-02-17 11:59:30"
-    role 0
+    oauth_expires_at "018-02-27 15:35:40 -0700"
+    refresh_token ENV['refresh_token']
     calendar ENV['calendar']
   end
 end
