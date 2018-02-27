b5bb05bbb800db2461df86474ba5b3597a518236
Anna Lewis <anna.noel.lewis@gmail.com>
2018-02-26 20:05:46 -0700

    Save refresh token on user

--- db/migrate/20180223230148_add_calendar_to_user.rb
+++
@@ -1,5 +0,0 @@
-class AddCalendarToUser < ActiveRecord::Migration[5.1]
-  def change
-    add_column :users, :calendar, :string
-  end
-end
--- db/migrate/20180227025956_create_user.rb
+++ db/migrate/20180227025956_create_user.rb
@@ -1,14 +1,13 @@
-class CreateUsers < ActiveRecord::Migration[5.1]
+class CreateUser < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
       t.string :provider
       t.string :uid
       t.string :name
       t.string :oauth_token
-      t.datetime :oauth_expires_at
-      t.integer :role, default: 0
-
-      t.timestamps
+      t.string :oauth_expires_at
+      t.string :refresh_token
+      t.string :calendar
     end
   end
 end
