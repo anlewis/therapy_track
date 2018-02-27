adea7779269b4bad6134ea7667271803413596f3
Anna Lewis <anna.noel.lewis@gmail.com>
2018-02-27 15:02:12 -0700

    Fix timezones

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -8,10 +8,8 @@ class AppointmentsController < ApplicationController
       params['summary'],
       params['location'],
       params['description'],
-      params['start_date'],
-      params['start_time'],
-      params['end_date'],
-      params['end_time']
+      params['start'].to_datetime.change(offset: "-0700"),
+      params['end'].to_datetime.change(offset: "-0700")
     )
     redirect_to appointments_url(calendar_id: current_user.calendar)
   end
