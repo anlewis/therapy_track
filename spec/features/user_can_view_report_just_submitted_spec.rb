6f166bdf0007637a38d6187ab7748a4d70ee1e46
Anna Lewis <anna.noel.lewis@gmail.com>
2018-02-28 12:14:13 -0700

    Update medical report partial

--- spec/features/user_can_view_report_just_submitted_spec.rb
+++ spec/features/user_can_view_report_just_submitted_spec.rb
@@ -15,5 +15,9 @@ feature 'User can view their generated wellness report' do
     expect(page).to have_content 'Wellness Report'
     expect(page).to have_content 'Basic Medical Information'
     expect(page).to have_content "O2 Saturation: #{medical_report.oxygen} %"
+    expect(page).to have_content "Temperature: #{medical_report.temperature} F"
+    expect(page).to have_content "Weight: #{medical_report.weight} Ibs"
+    expect(page).to have_content "Blood Pressure: #{medical_report.systolic}/#{medical_report.diastolic} mmHg"
+    expect(page).to have_content "Appointment Notes: #{medical_report.notes}"
   end
 end
