a57ee80f5077a5530e57f4f0b7f9191a6d7b2f90
Anna Lewis <anna.noel.lewis@gmail.com>
2018-02-27 19:47:32 -0700

    Formatting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,16 @@ module ApplicationHelper
     date_time.in_time_zone.strftime("%a, %-m/%d/%Y at %l:%M")
   end
 
+  def get_weekday(date_time)
+    date_time.in_time_zone.strftime("%A")
+  end
+
+  def get_date(date_time)
+    date_time.in_time_zone.strftime("%-m/%d/%Y")
+  end
+
+  def get_time(date_time)
+    date_time.in_time_zone.strftime("%l:%M %p")
+  end
+
 end
