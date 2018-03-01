ba5db1d719bb313769edb409125a0084208d0772
Anna Lewis <anna.noel.lewis@gmail.com>
2018-02-28 20:37:08 -0700

    Update navigation links; write test for about page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
   get 'signout', to: 'sessions#destroy', as: 'signout'
+  get 'about', to: 'about#index'
 
   resources :sessions, only: [:create, :destroy]
   resource :home, only: [:show]
