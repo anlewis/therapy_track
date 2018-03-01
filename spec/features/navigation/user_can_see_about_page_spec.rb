e60205419c2738b59222c3d3f8dba94bfe96f566
Anna Lewis <anna.noel.lewis@gmail.com>
2018-02-28 20:37:08 -0700

    Update navigation links; write test for about page

---
+++ spec/features/navigation/user_can_see_about_page_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+feature "A user can see an about page" do
+  scenario "as a logged in user" do
+    user = create(:user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit '/'
+    click_on 'About'
+
+    expect(current_path).to eq about_path
+    expect(page).to have_content 'About TherapyTrack'
+  end
+end
