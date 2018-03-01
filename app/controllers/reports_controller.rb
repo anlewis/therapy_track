dfad84f5baafccd24c938e767c2145f04a2f1ec3
Anna Lewis <anna.noel.lewis@gmail.com>
2018-02-28 21:20:24 -0700

    Create test and make pass for reports index; style

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,11 +1,14 @@
 class ReportsController < ApplicationController
-  def create
-    report = Report.create
-    current_user.reports << report
-    redirect_to new_report_medical_report_path(report)
+  def index
   end
 
   def show
     @report = Report.find(params[:id])
+  end
+
+  def create
+    report = Report.create
+    current_user.reports << report
+    redirect_to new_report_medical_report_path(report)
   end
 end
