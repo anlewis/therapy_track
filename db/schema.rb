b1e605e07cf2acdf92bca11bd8e2666cdf440c0b
Anna Lewis <anna.noel.lewis@gmail.com>
2018-03-05 13:00:08 -0600

    Outline tests, update env refresh_token

--- db/schema.rb
+++ db/schema.rb
@@ -10,41 +10,42 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_180_228_183_919) do
+ActiveRecord::Schema.define(version: 20180228183919) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'medical_reports', force: :cascade do |t|
-    t.bigint 'report_id'
-    t.integer 'oxygen'
-    t.integer 'temperature'
-    t.integer 'weight'
-    t.integer 'systolic'
-    t.integer 'diastolic'
-    t.text 'notes'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['report_id'], name: 'index_medical_reports_on_report_id'
+  create_table "medical_reports", force: :cascade do |t|
+    t.bigint "report_id"
+    t.integer "oxygen"
+    t.integer "temperature"
+    t.integer "weight"
+    t.integer "systolic"
+    t.integer "diastolic"
+    t.text "notes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["report_id"], name: "index_medical_reports_on_report_id"
   end
 
-  create_table 'reports', force: :cascade do |t|
-    t.bigint 'user_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.integer 'status', default: 0
-    t.index ['user_id'], name: 'index_reports_on_user_id'
+  create_table "reports", force: :cascade do |t|
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "status", default: 0
+    t.index ["user_id"], name: "index_reports_on_user_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'provider'
-    t.string 'uid'
-    t.string 'name'
-    t.string 'oauth_token'
-    t.string 'oauth_expires_at'
-    t.string 'refresh_token'
-    t.string 'calendar'
+  create_table "users", force: :cascade do |t|
+    t.string "provider"
+    t.string "uid"
+    t.string "name"
+    t.string "oauth_token"
+    t.string "oauth_expires_at"
+    t.string "refresh_token"
+    t.string "calendar"
   end
 
-  add_foreign_key 'medical_reports', 'reports'
-  add_foreign_key 'reports', 'users'
+  add_foreign_key "medical_reports", "reports"
+  add_foreign_key "reports", "users"
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :user do
-    provider 'google_oauth2'
+    provider ENV['provider']
     uid ENV['uid']
-    name 'Anna Lewis'
+    name ENV['name']
     oauth_token ENV['oauth_token']
-    oauth_expires_at '018-02-27 15:35:40 -0700'
+    oauth_expires_at ENV['oauth_expires_at']
     refresh_token ENV['refresh_token']
     calendar ENV['calendar']
   end
